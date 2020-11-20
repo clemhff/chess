7266ec07fc17aac029a84beb92e7fff53f6196b7
clemhff <clemthebest57@gmail.com>
2020-11-20 12:46:29 +0100

    logic pieces moves

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -42,14 +42,14 @@ function GameBoard() {
     let lineMove2 = Array(8).fill().map((_, idx) => [0,(-idx)-1]);
     let lineMove3 = Array(8).fill().map((_, idx) => [idx+1,0]);
     let lineMove4 = Array(8).fill().map((_, idx) => [(-idx)-1,0]);
-    let lineMove = [...lineMove1, ...lineMove2, ...lineMove3, ...lineMove4];
+    let lineMove = [lineMove1, lineMove2, lineMove3, lineMove4];
     //console.log(lineMove);
 
     let diagonalMove1 = Array(8).fill().map((_, idx) => [idx+1,idx+1]);
     let diagonalMove2 = Array(8).fill().map((_, idx) => [idx+1,(-idx)-1]);
     let diagonalMove3 = Array(8).fill().map((_, idx) => [(-idx)-1,idx+1]);
     let diagonalMove4 = Array(8).fill().map((_, idx) => [(-idx)-1,(-idx)-1]);
-    let diagonalMove = [...diagonalMove1, ...diagonalMove2, ...diagonalMove3, ...diagonalMove4];
+    let diagonalMove = [diagonalMove1, diagonalMove2, diagonalMove3, diagonalMove4];
 
     let queenMove = [...lineMove, ...diagonalMove];
 
@@ -57,30 +57,73 @@ function GameBoard() {
       case 'rook':
         return lineMove;
       case 'knight':
-        return( [[2,1],[1,2],[(-2),1],[(-1),2],[2,(-1)],[1,(-2)],[(-2),(-1)],[(-1),(-2)]] );
+        return( [[[2,1],[1,2],[(-2),1],[(-1),2],[2,(-1)],[1,(-2)],[(-2),(-1)],[(-1),(-2)]]] );
       case 'bishop':
         return diagonalMove;
       case 'queen':
         return queenMove;
+      case 'king':
+        return ( [[[1,0],[0,1],[(-1),0],[0,(-1)],[1,1],[(-1),(-1)],[(-1),1],[1,(-1)]]] );
       default:
         return [];
     }
   }
 
   function deleteOutMoves(moves, x, y) {
-    let addMoveToPosition = moves.map(move => [x + move[0], y + move[1]]);
-    let filterOut = addMoveToPosition.filter(positionPossible => {
-      let boolFilter = (positionPossible[0] < 8 && positionPossible[0] >= 0 && positionPossible[1] < 8 && positionPossible[1] >= 0) ? true : false
-      //console.log(x,' ',y,' ', positionPossible[0], y + positionPossible[1]);
-      return boolFilter === true;
+
+    let addMoveToPosition = moves.map(catMoves => {
+      let unitMove = catMoves.map(catMove => [x + catMove[0], y + catMove[1]]);
+      return unitMove;
     });
+
+    let filterOut = addMoveToPosition.map(catMoves => {
+      let unitMove = catMoves.filter(positionPossible => {
+        let boolFilter = (positionPossible[0] < 8 && positionPossible[0] >= 0 && positionPossible[1] < 8 && positionPossible[1] >= 0) ? true : false
+        //console.log(x,' ',y,' ', positionPossible[0], y + positionPossible[1]);
+        return boolFilter === true;
+      });
+      return unitMove;
+    });
+
     return filterOut;
+  }
+
+  function pieceBlocked(squares, arrayMove, obj) {
+
+
+
+    let moveNotBlocked = arrayMove.map(catMoves => {
+
+      let filter = [];
+      let bool = false;
+      let stopGoingFurther = () => bool = true;
+      for (let i = 0; i < catMoves.length; i++){
+        //console.log(catMoves[0][0]);
+
+        if(squares[catMoves[i][0]][catMoves[i][1]].piece === null && bool === false){
+          filter.push(catMoves[i]);
+        }
+        else if (squares[catMoves[i][0]][catMoves[i][1]].piece !== null && squares[catMoves[i][0]][catMoves[i][1]].team !== obj.team && bool === false){
+          filter.push(catMoves[i]);
+          stopGoingFurther() ;
+        }
+        else {stopGoingFurther() ;}
+
+
+      }
+      return filter;
+    });
+
+    return moveNotBlocked;
   }
 
   function squarePossible(obj) {
     let moves = pieceMove(obj.piece);
     let outofBoard = deleteOutMoves(moves, obj.x, obj.y);
-    return outofBoard;
+    //console.log(outofBoard);
+    let blockedFilter = pieceBlocked(squares,outofBoard, obj);
+
+    return blockedFilter;
 
   }
 
